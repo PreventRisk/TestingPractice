9828c2df54950356d5a10c15f4f8645a01c3e1a7
Carlitoshsh <caragomezher@unal.edu.co>
2015-10-18 14:31:05 -0500

    Se realizan test unitarios para los campos nombre, edad y genero de Usuario. Los pruebas que fueron comentadas indican que no fueron exitosas y que se debe revisar la clase Usuario, para confirmar que las restricciones son correctas.

--- test/unit/testingpractice/UsuarioControllerSpec.groovy
+++ test/unit/testingpractice/UsuarioControllerSpec.groovy
@@ -8,4 +8,64 @@ import spock.lang.*
 @Mock(Usuario)
 class UsuarioControllerSpec extends Specification {
 
+    def "Prueba de nombre"(){
+        when:
+        def usuario = new Usuario(apellido: "Gomez", edad: 20, genero: "M", cedula: 1022543545)
+        usuario.nombre = nombre
+        usuario.validate()
+
+        then:
+        usuario.hasErrors() == !valido
+
+        where:
+        nombre   | valido
+        "LD" | false // Minimo son tres caracteres
+        "123456789012345678901234567890123456789012345678901" | false // No puede exceder los 50 caracteres
+        //"Carlos Alfonso" | true
+        //"Carlos" | true
+        //"Joe" | true
+        //"12345678901234567890123456789012345678901234567890" | true
+    }
+
+    def "Prueba apellido"(){
+
+    }
+
+    def "Prueba de edad"(){
+        when:
+        def usuario = new Usuario(nombre: "Camila", apellido: "Lopez", genero: "F", cedula: 1023443432)
+        usuario.edad = edad
+        usuario.validate()
+
+        then:
+        usuario.hasErrors() == !valid
+
+        where:
+        edad | valid
+        14   | false // Edad menor a 18
+        18   | true // Edad mínima permitida
+        //17   | false // Edad menor a 18, (es el limite -1)
+    }
+
+
+    def "Prueba genero"(){
+        when:
+        def usuario = new Usuario(nombre: "Fulanito", apellido: "Detal", edad: 20, cedula: 1023323323)
+        usuario.genero = genero
+        usuario.validate()
+
+        then:
+        usuario.hasErrors() == !valid
+
+        where:
+        genero | valid
+        //"M"    | true // M y F son las unicos valores que puede tomar
+        //"K"    | false
+        "F"    | true
+        //"m"    | false
+    }
+
+    def "Prueba cedula"(){
+
+    }
 }
